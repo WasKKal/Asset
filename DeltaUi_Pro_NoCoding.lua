20a037eeb8dbd6c3bb0573d00595387087f3228c
WasKKal <kkalalwe@icloud.com>
2026-09-06 01:59:12 +0800

    chore(loader): pin deobfuscator CDN to @7f487ef0
    锁定去注释后的 v1.0.0

--- DeltaUi_Pro_NoCoding.lua
+++ DeltaUi_Pro_NoCoding.lua
@@ -3199,7 +3199,7 @@ local PRESET_PAGES = {
         titleKey = "preset_deobfuscator",
         descKey = "preset_deobfuscator_desc",
         icon = "shield-check",
-        url = "https://cdn.jsdelivr.net/gh/WasKKal/Asset@8dc4ed051575265ed4a6ed28eb8b33fd0e1c2f66/deltaui/deobfuscator.lua",
+        url = "https://cdn.jsdelivr.net/gh/WasKKal/Asset@7f487ef0/deltaui/deobfuscator.lua",
         unsafe = true,
         version = "1.0.0",
     },

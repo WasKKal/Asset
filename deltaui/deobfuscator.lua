593b692dcd785891972ee2f74c291ecad096d9a0
WasKKal <kkalalwe@icloud.com>
2026-09-07 22:53:36 +0800

    chore: bump page version to 1.0.0

--- deltaui/deobfuscator.lua
+++ deltaui/deobfuscator.lua
@@ -3,7 +3,7 @@ DeltaPageInfo = {
     title = "反混淆工具",
     icon = "shield-check",
     dataFolder = "deobfuscator",
-    version = "1.1.0",
+    version = "1.0.0",
 }
 local pageInfo = DeltaPageInfo
 
